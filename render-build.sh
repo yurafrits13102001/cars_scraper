06e02feab8c265d71a804080cf15f451971d5340
yurafrits13102001 <118067873+yurafrits13102001@users.noreply.github.com>
2026-02-09 23:16:14 +0200

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,4 @@
-# #!/usr/bin/env bash
-# pip install -r requirements.txt
-# playwright install chromium pip install -r requirements.txt && playwright install chromium
 #!/usr/bin/env bash
 pip install -r requirements.txt
-playwright install chromium
+playwright install chromium pip install -r requirements.txt && playwright install chromium
+
